0cb68306845896a7e56fdd2e3f2c99733757619e
wangguowei <wangguowei0126@163.com>
2020-07-09 18:31:56 +0800

    tpshop商城

--- run_suite.py
+++ run_suite.py
@@ -2,12 +2,10 @@ import unittest
 import time
 from case.code_login_order import CodeLoginOrder
 from tools.HTMLTestRunner import HTMLTestRunner
+
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(CodeLoginOrder))
-# with open("report/login{}.html".format(time.strftime('%Y%m%d_%H%M%S')), "wb", ) as f:
-with open("report/login.html", "wb") as f:
-
+with open("report/login{}.html".format(time.strftime('%Y%m%d_%H%M%S')), "wb", ) as f:
     runner = HTMLTestRunner(stream=f)
-# unittest.TextTestRunner().run(suite)
+    # unittest.TextTestRunner().run(suite)
     runner.run(suite)
-
